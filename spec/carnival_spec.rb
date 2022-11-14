ff9d774650a6d73316c4242923dea1f63cb00045
KaraJH <karas.jones@yahoo.com>
2022-11-14 11:41:19 -0700

    Creates total revenue method

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -25,20 +25,41 @@ RSpec.describe Carnival do
   end
 
   describe '#popular_ride' do
-    it 'can show the most popular ride at the carnival' do
+    xit 'can show the most popular ride at the carnival' do
+      carnival = Carnival.new('10 hours')
       ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
       ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
       visitor1 = Visitor.new('Bruce', 54, '$10')
       visitor2 = Visitor.new('Tucker', 36, '$5')
       visitor3 = Visitor.new('Penny', 64, '$15')
-      visitor1.add_preference(:gentle)
-      visitor2.add_preference(:gentle)
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
       ride1.board_rider(visitor1)
       ride1.board_rider(visitor2)
       ride1.board_rider(visitor3)
       ride2.board_rider(visitor1)
 
       expect(carnival.popular_ride).to eq([ride1])
+    end
+  end
+
+  describe '#total_revenue' do
+    it 'can show the total revenue earned from al the rides' do
+      carnival = Carnival.new('10 hours')
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      expect(carnival.total_revenue).to eq(0)
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor3)
+      ride2.board_rider(visitor1)
+      expect(carnival.total_revenue).to eq(8)
+
     end
   end
 end
