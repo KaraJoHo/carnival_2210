ec60446bdf88ba342a75222443e66f5b36ab1bc0
KaraJH <karas.jones@yahoo.com>
2022-11-14 11:20:40 -0700

    Creates add ride method test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -10,4 +10,35 @@ RSpec.describe Carnival do
     expect(carnival.duration).to eq('10 hours')
     expect(carnival.rides).to eq([])
   end
+
+  describe '#add_ride' do
+    it 'adds a ride to the carnival rides array' do
+      carnival = Carnival.new('10 hours')
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+
+      expect(carnival.rides).to eq([])
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      expect(carnival.rides).to eq([ride1, ride2])
+    end
+  end
+
+  describe '#popular_ride' do
+    xit 'can show the most popular ride at the carnival' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor3)
+      ride2.board_rider(visitor1)
+
+      expect
+    end
+  end
 end
