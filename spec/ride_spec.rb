9517965201a41209e3ea2ade26e433ad7818df08
KaraJH <karas.jones@yahoo.com>
2022-11-14 09:50:10 -0700

    Creates test for board rider method

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -11,5 +11,23 @@ RSpec.describe Ride do
     expect(ride1.admission_fee).to eq(1)
     expect(ride1.excitement).to eq(:gentle)
     expect(ride1.total_revenue).to eq(0)
+    exoect(ride1.rider_log).to eq({})
+  end
+
+  describe '#board_rider' do
+    it 'has an hash of riders and adds the visitors and amount of times they boarded' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor1)
+
+      expected = {visitor1 => 2, visitor2 => 1}
+      expect(ride1.rider_log).to eq(expected)
+
+    end
   end
 end
