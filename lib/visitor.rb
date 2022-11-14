63d5ff236485a98e70ca17db930d8906746b3887
KaraJH <karas.jones@yahoo.com>
2022-11-14 09:38:37 -0700

    Creates tall enough method with almost passing test

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,14 +1,28 @@
 class Visitor
-  attr_reader :name, :height, :spending_money, :preferences
+  attr_reader :name, :height, :spending_money, :preferences, :visitors
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
     @spending_money = spending_money.delete('$').to_i
     @preferences =[]
+    @visitors = []
   end
 
   def add_preference(preference)
     @preferences << preference
   end
+
+  def add_visitors(visitor)
+    @visitors << visitor
+  end
+
+  def tall_enough?(threshold)
+     @visitors.map do |visitor|
+      if visitor.height >= threshold
+        true
+      end
+    end.flatten
+  end
+
 end
