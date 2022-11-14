dc68c0cb6f8f3e8aa81b81828680caec7d8c266f
KaraJH <karas.jones@yahoo.com>
2022-11-14 11:51:08 -0700

    Creates most profitable method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,4 +1,3 @@
-#left comments to show thought process
 class Carnival
   attr_reader :duration, :rides
 
@@ -12,17 +11,6 @@ class Carnival
   end
 
   def popular_ride
-    #find the ride that had the most riders
-    #we have rides in the rides array
-    #each ride has a log which has the number of times
-    #it was boarded
-    #find the max number of times the ride was boarded
-    #return that ride as the popular one
-
-    #first idea is to make an array to collect the popular_ride
-    #iterate through rides and check if any logs are empty
-    #if they are not empty, shovel them in to the array
-    #check that array for the highest value using max by
 
     popular = []
       @rides.each do |ride|
@@ -30,22 +18,19 @@ class Carnival
           popular << ride
         end
       end
-      popular.max_by do |ride|
-        require 'pry' ;binding.pry
-        ride.rider_log.count
+        popular.max_by do |ride|
+          ride.rider_log.count
       end
     end
 
-    #second idea, just use max by to find the highest value
-    #by the rider count
-    #
-    # @rides.max_by do |ride|
-    #   #require 'pry' ;binding.pry
-    #   ride.rider_log.count
-    # end
-
     def total_revenue
       @rides.sum do |ride|
+        ride.total_revenue
+      end
+    end
+
+    def most_profitable_ride
+      @rides.max_by do |ride|
         ride.total_revenue
       end
     end
