20df9de1078bca70fb85f6c7fc5337fec36ad946
KaraJH <karas.jones@yahoo.com>
2022-11-14 09:11:41 -0700

    Fixes syntax error in test

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe Visitor do
     expect(visitor1.preferences).to eq([])
   end
 
-  describe '#add_preferences' do
+  describe '#add_preference' do
     it 'adds preferences to the visitor array' do
       visitor1 = Visitor.new('Bruce', 54, '$10')
 
-      expect(visitor1.preferences).to eq([]))
+      expect(visitor1.preferences).to eq([])
       visitor1.add_preference(:gentle)
       visitor1.add_preference(:water)
       expect(visitor1.preferences).to eq([:gentle, :water])
